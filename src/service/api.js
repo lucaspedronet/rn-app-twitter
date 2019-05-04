bfab1ee580d184b6d46b485c9d903254c497f1a3
lucaspedronet <lucaspedrolbg@gmail.com>
2019-05-04 02:02:09 -0300

    Correções no api.js ao 'http://10.0.3.2:3000'

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,8 @@
+/* eslint-disable linebreak-style */
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://10.0.3.2:3000',
+  baseURL: 'http://10.0.3.2:3000',
 });
 
 export default api;
